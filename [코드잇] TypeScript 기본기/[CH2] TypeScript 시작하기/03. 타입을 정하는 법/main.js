85feca20a534f2f5c112076e9c4e7de9ce2f9bba
Seokyung <xuejd17@gmail.com>
2025-04-18 16:01:06 +0900

    📝 [2-03] 타입을 정하는 법

---
+++ [코드잇] TypeScript 기본기/[CH2] TypeScript 시작하기/03. 타입을 정하는 법/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let size = 100;
+size = 200; // 정상 작동
+// size = "abc"; // error: 타입 오류 발생
+console.log(typeof size); // number
+console.log(size); // 200
---
+++ [코드잇] TypeScript 기본기/[CH2] TypeScript 시작하기/03. 타입을 정하는 법/main.ts
@@ -0,0 +1,6 @@
+let size: number = 100;
+size = 200; // 정상 작동
+// size = "abc"; // error: 타입 오류 발생
+
+console.log(typeof size); // number
+console.log(size); // 200
