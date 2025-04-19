a220ebce0e5a43499fe23a363c715beb1b839524
Seokyung <xuejd17@gmail.com>
2025-04-19 20:33:46 +0900

    📝 [5] 빌드된 js 파일들 업데이트

--- [코드잇] TypeScript 기본기/[CH5] 그 밖의 타입들/04. Union 타입/main.js
+++ [코드잇] TypeScript 기본기/[CH5] 그 밖의 타입들/04. Union 타입/main.js
@@ -6,11 +6,11 @@ var ClothingSize;
     ClothingSize["L"] = "L";
     ClothingSize["XL"] = "XL";
 })(ClothingSize || (ClothingSize = {}));
-// Union 타입: A타입 | B타입 (두 타입 다 파라미터로 쓸 수 있음)
+// Union 타입(|): A타입 | B타입 (두 타입 다 파라미터로 쓸 수 있음)
 const printSizes = (product) => {
     const availableSizes = product.sizes.join(", ");
     console.log(`${product.name}의 사이즈: ${availableSizes}`);
-    // if ... in ...: 특정한 타입에 대한 처리가 필요한 경우 사용
+    // if (... in ...) {} : 특정한 타입에 대한 처리가 필요한 경우 사용
     if ("color" in product) {
         console.log(`색상: ${product.color}`);
     }
